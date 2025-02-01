2e0b49e6df7e931d9e9a8109ba251f6471cb0e79
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2025-02-01 15:13:44 +0000

    [Finish]時刻取得機能完成

--- app/javascript/packs/scripts/time_script.js
+++ app/javascript/packs/scripts/time_script.js
@@ -1,11 +1,21 @@
 const clock = document.querySelector('#clock');
 
+const layout = (data) => {
+    let num;
+    if(data < 10){
+        num = '0' + data;
+    }else{
+        num = data;
+    }
+    return num;
+};
+
 const getTime = () => {
     let nowTimes = new Date();
-    let nowHours = nowTimes.getHours();
-    let nowMinutes = nowTimes.getMinutes();
-    let nowSeconds = nowTimes.getSeconds();
+    let nowHours = layout(nowTimes.getHours());
+    let nowMinutes = layout(nowTimes.getMinutes());
+    let nowSeconds = layout(nowTimes.getSeconds());
     clock.textContent = `${nowHours}:${nowMinutes}:${nowSeconds}`;
 };
 
-setInterval(getTime(), 1000);
+setInterval(getTime, 1000);
