e21649cef1c3a127eeed10f6ef97b61b1c0dd56d
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2025-02-02 13:08:39 +0000

    [Add]日付取得機能完成

--- app/javascript/packs/scripts/time_script.js
+++ app/javascript/packs/scripts/time_script.js
@@ -1,4 +1,5 @@
 const clock = document.querySelector('#clock');
+const date = document.querySelector('#date');
 
 const layout = (data) => {
     let num;
@@ -10,12 +11,25 @@ const layout = (data) => {
     return num;
 };
 
+
 const getTime = () => {
     let nowTimes = new Date();
+    
     let nowHours = layout(nowTimes.getHours());
     let nowMinutes = layout(nowTimes.getMinutes());
     let nowSeconds = layout(nowTimes.getSeconds());
     clock.textContent = `${nowHours}:${nowMinutes}:${nowSeconds}`;
 };
 
+
+const getDay = () => {
+    let nowTimes = new Date();
+    
+    let nowYears = layout(nowTimes.getFullYear());
+    let nowMonthes = layout(nowTimes.getMonth() + 1);
+    let nowDates = layout(nowTimes.getDate());
+    date.textContent = `${nowYears}年${nowMonthes}月${nowDates}日`;
+};
+
+setInterval(getDay, 1000);
 setInterval(getTime, 1000);
