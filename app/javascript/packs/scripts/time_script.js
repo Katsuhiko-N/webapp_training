4fe9f2f43442853c6ee449e501f9aec796755104
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2025-02-03 14:41:22 +0000

    [Add]円グラフ状の時計追加

--- app/javascript/packs/scripts/time_script.js
+++ app/javascript/packs/scripts/time_script.js
@@ -11,10 +11,8 @@ const layout = (data) => {
     return num;
 };
 
-
 const getTime = () => {
     let nowTimes = new Date();
-    
     let nowHours = layout(nowTimes.getHours());
     let nowMinutes = layout(nowTimes.getMinutes());
     let nowSeconds = layout(nowTimes.getSeconds());
@@ -24,7 +22,6 @@ const getTime = () => {
 
 const getDay = () => {
     let nowTimes = new Date();
-    
     let nowYears = layout(nowTimes.getFullYear());
     let nowMonthes = layout(nowTimes.getMonth() + 1);
     let nowDates = layout(nowTimes.getDate());
@@ -33,3 +30,44 @@ const getDay = () => {
 
 setInterval(getDay, 1000);
 setInterval(getTime, 1000);
+
+
+
+// ここから時計グラフ
+const c_hours = document.querySelector('#hours');
+const c_minutes = document.querySelector('#minutes');
+const c_seconds = document.querySelector('#seconds');
+
+
+// 円の色
+c_hours.style.stroke = "dodgerblue";
+c_minutes.style.stroke = "springgreen";
+c_seconds.style.stroke = "#ff7b00";
+
+// strokeDasharrayの中身は割合、円周長の順
+
+const hour_circle = () => {
+    let nowTimes = new Date();
+    let nowHours = layout(nowTimes.getHours());
+    let hours_area = 220 * (nowHours / 24);
+    c_hours.style.strokeDasharray = `${hours_area}, 220`;
+};
+
+const minute_circle = () => {
+    let nowTimes = new Date();
+    let nowMinutes = layout(nowTimes.getMinutes());
+    let minutes_area = 220 * (nowMinutes / 60);
+    c_minutes.style.strokeDasharray = `${minutes_area}, 220`;
+};
+
+const second_circle = () => {
+    let nowTimes = new Date();
+    let nowSeconds = layout(nowTimes.getSeconds());
+    let seconds_area = 220 * (nowSeconds / 60);
+    c_seconds.style.strokeDasharray = `${seconds_area}, 220`;
+};
+
+
+setInterval(hour_circle, 60000);
+setInterval(minute_circle, 1000);
+setInterval(second_circle, 1000);
